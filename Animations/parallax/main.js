9fb5b2efb408d32d9001a13e3bc7f505c2091cdc
knowojewski <xsardas.geza@gmail.com>
2020-06-13 10:30:19 +0200

    JS Parallax effect

---
+++ Animations/parallax/main.js
@@ -0,0 +1,14 @@
+const parallax = (element, distance, speed) => {
+    const item = document.querySelector(element);
+    item.style.transform = `translateY(${distance * speed}px)`;
+};
+
+
+window.addEventListener('scroll', function() {
+    parallax('.left-shape', window.scrollY, 0.2);
+    parallax('.right-shape', window.scrollY, 0.3);
+    parallax('.forest', window.scrollY, 0.4);
+    parallax('.moon', window.scrollY, 0.5);
+    parallax('h1', window.scrollY, 0.25);
+    parallax('h3', window.scrollY, 0.25);
+});
