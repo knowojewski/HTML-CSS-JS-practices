b37e39919d1a726e37766fdb17603d7f98429620
knowojewski <xsardas.geza@gmail.com>
2020-06-29 18:16:11 +0200

    Memory game, unfinished styling

---
+++ JavaScript/memoryGame/icons.js
@@ -0,0 +1,31 @@
+const icons = [
+    {
+        icon: '<img src="./images/Warrior.PNG" alt="image">',
+        number: 0
+    },
+
+    {
+        icon: '<img src="./images/deamonPrince.PNG" alt="image">',
+        number: 1
+    },
+
+    {
+        icon: '<img src="./images/Knight.PNG" alt="image">',
+        number: 2
+    },
+
+    {
+        icon: '<img src="./images/LordOnMount.PNG" alt="image">',
+        number: 3
+    },
+
+    {
+        icon: '<img src="./images/Sorcerer.PNG" alt="image">',
+        number: 4
+    },
+
+    {
+        icon: '<img src="./images/Marauder.PNG" alt="image">',
+        number: 5
+    }
+]
