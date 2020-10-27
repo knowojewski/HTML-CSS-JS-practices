259b44783056fc779f5942ecc492fa6f762cc8a4
knowojewski <xsardas.geza@gmail.com>
2020-10-27 10:35:27 +0100

    Random balls

--- JavaScript/ColorBalls/app.js
+++ JavaScript/ColorBalls/app.js
@@ -5,11 +5,17 @@ btn.addEventListener('click', generateBall);
 
 function generateBall() {
     let ball = document.createElement('div');
+    let heightWidth = Math.floor(Math.random()*50);
+    let top = Math.floor(Math.random()*800);
+    let rgb1 = Math.floor(Math.random()*255);
+    let rgb2 = Math.floor(Math.random()*255);
+    let rgb3 = Math.floor(Math.random()*255);
+
     ball.classList.add('ball', 'animate');
-    ball.style.backgroundColor = `rgb(126, 145, 255)`;
-    ball.style.height = `50px`;
-    ball.style.width = `50px`;
-    ball.style.top = `800px`;
+    ball.style.backgroundColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
+    ball.style.height = `${heightWidth}px`;
+    ball.style.width = `${heightWidth}px`;
+    ball.style.top = `${top}px`;
 
 
     board.appendChild(ball);
